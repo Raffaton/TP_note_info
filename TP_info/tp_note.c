62541b8a58aabad7272a0ea6c6e031bddb21953b
Raphael <raphael.souchet.85@gmail.com>
2024-10-21 10:29:13 +0200

    ex 2 fini !

--- TP_info/tp_note.c
+++ TP_info/tp_note.c
@@ -4,7 +4,7 @@ void creer_grille(int taille, int grille[9][9])
 {
     for(int i = 0; i < taille; i++)
     {
-        for(int j = 0; j < taille; j ++)
+        for(int j = 0; j < taille; j++)
         {
             grille[i][j] = 0;
         }
@@ -13,7 +13,7 @@ void creer_grille(int taille, int grille[9][9])
 
 void afficher_grille(int taille, int grille[9][9])
 {
-    for (int i = 0; i < taille; i ++)
+    for (int i = 0; i < taille; i++)
     {
         for (int j = 0; j < taille; j++)
         {
@@ -23,17 +23,51 @@ void afficher_grille(int taille, int grille[9][9])
     }
 }
 
+void modifier_grille(int ligne, int colonne, int grille[9][9], int nombre)
+{
+    if (ligne >= 0 && ligne < 9 && colonne >= 0 && colonne < 9)
+    {
+        grille[ligne][colonne] = nombre;
+    }
+}
+
 int main()
 {
     int taille = 10;
     int grille[9][9];
-    while (taille > 9)
+    int envie = 1;
+
+    while (taille > 9 || taille < 1)
     {
-        printf("Quel est la taille du carre ? ");
+        printf("Quelle est la taille du carre (entre 1 et 9) ? ");
         scanf("%d", &taille);
     }
+
     creer_grille(taille, grille);
     afficher_grille(taille, grille);
+
+    while (1)
+    {
+        printf("Voulez vous changer une valeur de tableau (0 pour NON / 1 pour OUI) : ");
+        scanf("%d", &envie);
+        
+        if (envie == 1)
+        {
+            int ligne;
+            int colonne;
+            int nombre;
+
+            printf("Indiquez la ligne, colone et la nouvelle valeur que vous voulez modifier : ");
+            scanf("%d %d %d", &ligne, &colonne, &nombre);
+            modifier_grille(ligne-1, colonne-1, grille, nombre);
+            printf("\nGrille apres modification:\n");
+            afficher_grille(taille, grille);
+        }
+        else
+        {
+            break;
+        }
+    }
 
     return 0;
 }
